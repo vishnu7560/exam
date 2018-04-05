f3d7b587175a55a5434a9d2ea1bd898e2c035256
vishnu <vichu.vmr.10@gmail.com>
2018-04-05 14:55:18 +0530

    first.sh created

---
+++ first.sh
@@ -0,0 +1,13 @@
+echo "Enter first number"
+read a
+echo "Enter second number"
+read b
+sum=$((a+b))
+echo "sum:$sum"
+sub=$((a-b))
+echo "sub:$sub"
+mul=$((a*b))
+echo "mul:$mul"
+div=$((a/b))
+echo "div:$div"
+
